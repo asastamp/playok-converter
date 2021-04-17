04f33202a7ccfe512850c7793e603c65f7872d7a
5830032121-KN <n_kittiphan@hotmail.com>
2021-04-17 17:57:33 +0700

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <Main/>
+	<Main />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Main from './components/Main.vue';
+	import { Options, Vue } from "vue-class-component";
+	import Main from "./components/Main.vue";
 
-@Options({
-  components: {
-    Main,
-  },
-})
-export default class App extends Vue {}
+	@Options({
+		components: {
+			Main,
+		},
+	})
+	export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+	html {
+		height: 100%;
+	}
+	body {
+		margin: 0;
+		background: linear-gradient(to top right, #00aefc 0%, #00dcef 100%);
+		height: 100%;
+	}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
 </style>
